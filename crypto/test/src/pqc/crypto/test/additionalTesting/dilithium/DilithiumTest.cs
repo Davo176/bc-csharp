c63c8af3b59fe94958f49613075885ccb227ef4b
Davo176 <will@DESKTOP-8JTL123.localdomain>
2022-10-17 00:07:27 +1030

    Dilithium Interop done

--- crypto/test/src/pqc/crypto/test/additionalTesting/dilithium/DilithiumTest.cs
+++ crypto/test/src/pqc/crypto/test/additionalTesting/dilithium/DilithiumTest.cs
@@ -33,6 +33,31 @@ namespace Org.BouncyCastle.Pqc.Crypto.Tests.additionalTests
 
         private static readonly List<string> signTestVectorFileNames = new List<string>(signTestVectors.Keys);
 
+        private static readonly Dictionary<string, DilithiumParameters> interopKgVectors = new Dictionary<string, DilithiumParameters>()
+        {
+             { "keypairs_csharp_Dilithium2.rsp", DilithiumParameters.Dilithium2 },
+             { "keypairs_csharp_Dilithium3.rsp", DilithiumParameters.Dilithium3 },
+             { "keypairs_csharp_Dilithium5.rsp", DilithiumParameters.Dilithium5 }
+        };
+        private static readonly List<string> interopKgVectorsFileNames = new List<string>(interopKgVectors.Keys);
+
+        private static readonly Dictionary<string, DilithiumParameters> interopSignedVectors = new Dictionary<string, DilithiumParameters>()
+        {
+             { "keypairs_ref_Dilithium2.rsp", DilithiumParameters.Dilithium2 },
+             { "keypairs_ref_Dilithium3.rsp", DilithiumParameters.Dilithium3 },
+             { "keypairs_ref_Dilithium5.rsp", DilithiumParameters.Dilithium5 },
+        };
+        private static readonly List<string> interopSignedVectorsFileNames = new List<string>(interopSignedVectors.Keys);
+
+        private static readonly Dictionary<string, DilithiumParameters> interopCheckSignedVectors = new Dictionary<string, DilithiumParameters>()
+        {
+             { "signed_csharp_ref_Dilithium2.rsp", DilithiumParameters.Dilithium2 },
+             { "signed_csharp_ref_Dilithium3.rsp", DilithiumParameters.Dilithium3 },
+             { "signed_csharp_ref_Dilithium5.rsp", DilithiumParameters.Dilithium5 },
+        };
+        private static readonly List<string> interopCheckSignedVectorsFileNames = new List<string>(interopCheckSignedVectors.Keys);
+
+
         [TestCaseSource(nameof(addRandTestVectorFileNames))]
         [Parallelizable(ParallelScope.All)]
         public void TestFullVectors(string testVectorFile)
@@ -46,6 +71,29 @@ namespace Org.BouncyCastle.Pqc.Crypto.Tests.additionalTests
         {
             RunTest(signTestVectorFile,"pqc.crystals.dilithium.signVectors.",testSign,signTestVectors);
         }
+
+        [TestCaseSource(nameof(interopKgVectorsFileNames))]
+        [Parallelizable(ParallelScope.All)]
+        public void runCreateKeypairs(string interopFile)
+        {
+            CreateKeypairs(interopFile,interopKgVectors);
+        }
+
+        [TestCaseSource(nameof(interopSignedVectorsFileNames))]
+        [Parallelizable(ParallelScope.All)]
+        public void runCreateSigned(string interopFile)
+        {
+            RunTest(interopFile,"pqc.crystals.dilithium.interoperability.",CreateSigned,interopSignedVectors);
+        }
+
+        [TestCaseSource(nameof(interopCheckSignedVectorsFileNames))]
+        [Parallelizable(ParallelScope.All)]
+        public void runCheckSigned(string interopFile)
+        {
+            RunTest(interopFile,"pqc.crystals.dilithium.interoperability.",CheckSigned,interopCheckSignedVectors);
+        }
+
+
 
         private static void testSign(string name, IDictionary<string, string> buf,Dictionary<string, DilithiumParameters> paramDict)
         {
@@ -113,6 +161,109 @@ namespace Org.BouncyCastle.Pqc.Crypto.Tests.additionalTests
             Assert.True(Arrays.AreEqual(expectedSM, finalSM), "FAILED signature gen: " + name + " " + count);
 
             Assert.True(validSignature, "FAILED signature verify: " + name + " " + count);
+        }
+
+        public static void CreateKeypairs(string name,Dictionary<string, DilithiumParameters> paramDict){
+            byte[] seed = new byte[48];
+            byte[] entropy_input = new byte[48];
+            for (int i=0;i<48;i++){
+                entropy_input[i]=Convert.ToByte(i);
+            }
+            byte[] personalisation = new byte[48];
+            for (int i=48;i>0;i--){
+                personalisation[48-i]=Convert.ToByte(i);
+            }
+
+            NistSecureRandom random = new NistSecureRandom(entropy_input, personalisation);
+            DilithiumParameters parameters = paramDict[name];
+            string f1 = "../../../data/pqc/crystals/dilithium/interoperability/"+name;
+
+            string f1Contents = "";
+
+            for (int i=0;i<100;i++){
+                f1Contents += "count = "+i+"\n";
+                random.NextBytes(seed,0,48);
+                int messageLength = 33*(i+1);
+                byte[] message = new byte[messageLength];
+                f1Contents += "seed = " + Hex.ToHexString(seed)+"\n";
+                DilithiumKeyPairGenerator keysGenerator = new DilithiumKeyPairGenerator();
+                DilithiumKeyGenerationParameters generationParams = new DilithiumKeyGenerationParameters(random, parameters);
+                keysGenerator.Init(generationParams);
+                AsymmetricCipherKeyPair keys = keysGenerator.GenerateKeyPair();
+                random.NextBytes(message,0,messageLength);
+
+                DilithiumPublicKeyParameters publicKeyParams = (DilithiumPublicKeyParameters)keys.Public;
+                DilithiumPrivateKeyParameters privateKeyParams = (DilithiumPrivateKeyParameters)keys.Private;
+                f1Contents += "mlen = " + messageLength.ToString()+"\n";
+                f1Contents += "msg = " + Hex.ToHexString(message)+"\n";
+                f1Contents += "pk = " + Hex.ToHexString(publicKeyParams.GetEncoded())+"\n";
+                f1Contents += "sk = " + Hex.ToHexString(privateKeyParams.GetEncoded())+"\n";
+                f1Contents +="\n";
+            }
+            File.WriteAllText(f1,f1Contents);
+        }
+
+        public static void CreateSigned(string name, IDictionary<string, string> buf,Dictionary<string, DilithiumParameters> paramDict){
+            String count = buf["count"];
+            byte[] sk = Hex.Decode(buf["sk"]);
+            byte[] message = Hex.Decode(buf["msg"]);
+            
+        
+            byte[] seed = new byte[48];
+            byte[] entropy_input = new byte[48];
+            for (int i=0;i<48;i++){
+                entropy_input[i]=Convert.ToByte(i);
+            }
+
+            DilithiumParameters parameters = paramDict[name];
+            string f1 = "../../../data/pqc/crystals/dilithium/interoperability/signed_csharp_"+name.Substring("keypairs_ref_".Length,"Dilithium2".Length)+".rsp";
+
+            string f1Contents = "";
+            if (count=="0"){
+                File.WriteAllText(f1,f1Contents);
+            }
+  
+            f1Contents += "count = "+buf["count"]+"\n";
+            f1Contents += "pk = "+buf["pk"]+"\n";
+            NistSecureRandom random = new NistSecureRandom(entropy_input,null);
+            random.NextBytes(seed,0,48);
+
+            DilithiumPrivateKeyParameters privateKeyParams = new DilithiumPrivateKeyParameters(parameters,sk,random);
+            DilithiumSigner signer = new DilithiumSigner();
+
+            signer.Init(true,privateKeyParams);
+            
+            byte[] sigGenerated = signer.GenerateSignature(message);
+            byte[] attachedSig = Arrays.Concatenate(sigGenerated,message);
+            f1Contents += "mlen = " + buf["mlen"] + "\n";
+            f1Contents += "msg = " + buf["msg"] + "\n";
+            f1Contents += "smlen = " + attachedSig.Length.ToString() + "\n";
+            f1Contents += "sm = " + Hex.ToHexString(attachedSig) + "\n";
+            
+            f1Contents +="\n";
+
+            File.AppendAllText(f1,f1Contents);
+        }
+
+        public static void CheckSigned(string name, IDictionary<string, string> buf,Dictionary<string, DilithiumParameters> paramDict){
+            String count = buf["count"];
+            byte[] pk = Hex.Decode(buf["pk"]);
+            byte[] message = Hex.Decode(buf["msg"]);
+            byte[] sm = Hex.Decode(buf["sm"]);
+            int smlen = int.Parse(buf["smlen"]);
+            int mlen = int.Parse(buf["mlen"]);
+
+            DilithiumParameters parameters = paramDict[name];
+
+            DilithiumPublicKeyParameters publicKeyParams = new DilithiumPublicKeyParameters(parameters,pk);
+            DilithiumSigner verifier = new DilithiumSigner();
+
+            verifier.Init(false,publicKeyParams);
+            byte[] detachedSig = Arrays.CopyOfRange(sm,0,smlen-mlen);
+            
+            bool valid = verifier.VerifySignature(message,detachedSig);
+            Assert.True(valid, "FAILED signature verify: " + name + " " + count);
+            
         }
 
         public static void RunTest(string name,string partialLocation, Action<string,Dictionary<string,string>,Dictionary<string,DilithiumParameters>> testFunc,Dictionary<string,DilithiumParameters> parameters)
